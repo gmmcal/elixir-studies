c69d2591e6b197167ec0241764e962851da32d9b
Gustavo Cunha <gustavo@streetbees.com>
2017-09-05 22:17:50 +0100

    Create case tests

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -1,4 +1,14 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards, except: [shuffle: 1, create_hand: 1]
+
+  test "create_deck makes 12 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 12
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
 end
