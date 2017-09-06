9d5e2a4c340f2d55a01e0148fde35ecacc6964c3
Gustavo Cunha <gustavo@streetbees.com>
2017-09-06 09:56:26 +0100

    Fix travix build matrix

--- cards/mix.exs
+++ cards/mix.exs
@@ -5,7 +5,7 @@ defmodule Cards.Mixfile do
     [
       app: :cards,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: ">= 1.3",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
