80a00340fad7f436c9c674bae059612b1d510df4
Gustavo Cunha <gustavo@streetbees.com>
2017-09-25 21:28:12 +0100

    Add struct to manage hex of encrypted string

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,16 +12,18 @@ defmodule Identicon do
   end
 
   @doc """
-    Returns an array with a md5 identification of input
+    Returns an array with a hexdecimal identification of input
 
   ## Examples
 
       iex> Identicon.hash_input("banana")
-      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+      %Identicon.Image{hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]}
 
   """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
